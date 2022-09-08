2f34e01879a95bd61b7200e7639a57575e4373c6
LaraMerdol <laramerdol@gmail.com>
2022-09-08 23:35:30 +0300

    add edge and vertex buttons added

--- app/src/App.js
+++ app/src/App.js
@@ -1,68 +1,85 @@
-import "./App.css";
-import CytoscapeComponent from "react-cytoscapejs";
-import ReactCytoscape from 'react-cytoscape';
-import React, {useState} from "react";
-  const App = () => {
-    
-  const  [elements,setElements] = useState([
-    { data: { id: "one", label: "Node 1" }, position: { x: 200, y: 100 } },
-    { data: { id: "two", label: "Node 2" }, position: { x: 300, y: 100 } },
-    {
-      data: {
-        source: "one",
-        target: "two",
-        label: "Edge from Node1 to Node2",
-      },
-    },
-  ]);
+import React, {Fragment, useEffect, useRef, useState} from 'react';
+import cytoscape from 'cytoscape'
+import { Button } from 'primereact/button';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import './App.css';
 
-function addElements() {
-  let i = 0;
-  for (i = 0; i < 10; i++) {
-    elements.push({ data: { id:i, label:`Node${i}` } });
-  }
-  console.log(elements)
-  return elements;
+import { InputText } from 'primereact/inputtext';
+const App = () => {
+
+const [edge, setEdge] = useState("");
+const [vertex, setVertex] = useState("");
+const [list, setList] = useState([
+  { data: { id: 'a' } },
+  { data: { id: 'b' } },
+  {
+    data: {
+      id: 'ab',
+      source: 'a',
+      target: 'b'
+    }
+  }]);
+ const graphRef = useRef(null)
+
+ const drawGraph = () => {
+ let cy = cytoscape({
+  container: graphRef.current,
+  elements: list
+  });
+ }
+
+
+function addElement () {
+ console.log(drawGraph);
+ list.push( { data: { id: vertex} })
+ drawGraph();
 }
-    return (
-      <>
-      <CytoscapeComponent
-        elements={elements}
-        style={{
-          width: "800px",
-          height: "400px",
-          backgroundColor: "antiquewhite",
-          align: "center",
-        }}
-        stylesheet={[
-          {
-            selector: "node",
-            style: {
-              width: 20,
-              height: 20,
-              shape: "rectangle",
-            },
-          },
-          {
-            selector: "edge",
-            style: {
-              width: 5,
-            },
-          },
-        ]}
-        layout={{
-          name: "circle",
-        }}
-      />
-      <button onClick={addElements}
-       style={{
-          width: "70px",
-          height: "50px"}}
-          >
-            Add 100 Elements
-          </button>
-      </>
-    );
-  };
 
-export default App;
+function addEdge() {
+  console.log(drawGraph);
+  list.push(  {
+   data: {
+     id: edge+'b',
+     source: edge,
+     target: 'b'
+   }
+ })
+ console.log(list);
+ drawGraph();
+
+}
+
+ useEffect(() => {
+  drawGraph()
+ }, [])
+
+ return (
+  <Fragment>
+   <h2>Cytoscape Test</h2>
+   <div class="grid">
+   <div class="col-6">
+   <div ref={graphRef} style={{width: '80%', height: '80vh', backgroundColor:'antiquewhite'}}>
+   </div>
+   </div>
+   <div class="col-4">
+   <div class="grid">
+   <Button label="Save New Edge" icon="pi pi-check" iconPos="right" onClick={addEdge} />
+   <InputText value={edge} onChange={(e) => setEdge(e.target.value)} />
+   </div>
+   <br></br>
+   <br></br>
+   <div class="grid">
+   <Button label="Save New Vertex" icon="pi pi-check" iconPos="right" onClick={addElement} danger/>
+   <InputText value={vertex} onChange={(e) => setVertex(e.target.value)} />
+
+   </div>
+   </div>
+   </div>
+  </Fragment>
+  
+ )
+}
+
+export default App
